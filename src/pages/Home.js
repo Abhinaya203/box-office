5797c850c1555ecaa0574f5579b00c4d5917acaa
unknown <20311a05e0@sreenidhi.edu.in>
2022-06-19 22:52:15 +0530

    created fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 function Home() {
-  return <MainPageLayout>this is home</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  function onSearch() {
+    // https://api.tvmaze.com/search/shows?q=men
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  }
+
+  function onInputChange(ev) {
+    setInput(ev.target.value);
+  }
+
+  function onKeyDown(ev) {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  }
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 }
 
 export default Home;
